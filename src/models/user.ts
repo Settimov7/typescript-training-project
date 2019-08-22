1d39cb0b6f948a046066a6e7210478ed9f23f298
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-08-22 16:44:37 +0400

    move sync logic to specific class

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,6 @@
 import {Eventing} from './eventing';
-import axios, {AxiosResponse} from 'axios';
 
-interface Props {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
@@ -10,30 +9,13 @@ interface Props {
 export class User {
     public events = new Eventing();
 
-    constructor(private data: Props) {}
+    constructor(private data: UserProps) {}
 
     get(propName: string): string | number {
         return this.data[propName];
     }
 
-    set(newProps: Props): void {
+    set(newProps: UserProps): void {
         Object.assign(this.data, newProps);
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    save(): void {
-        const id = this.get('id');
-
-        if (id) {
-            axios.put(`http://localhost:3000/users/${this.get('id')}`, this.data);
-        } else {
-            axios.post('http://localhost:3000/users', this.data);
-        }
     }
 }
