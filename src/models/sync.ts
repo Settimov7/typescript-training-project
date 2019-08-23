82e61ed43de194fa46b8e917446aec525f35a0ed
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-08-23 11:06:26 +0400

    refactoring

--- src/models/sync.ts
+++ src/models/sync.ts
@@ -1,23 +1,24 @@
 import axios, { AxiosPromise } from 'axios';
 
 interface HasId {
-    id?: number;
+	id?: number;
 }
 
 export class Sync<T extends HasId> {
-    constructor(public rootUrl: string) {}
+	constructor(public rootUrl: string) {
+	}
 
-    fetch(id: number): AxiosPromise {
-        return axios.get(`${ this.rootUrl }/${ id }`);
-    }
+	fetch(id: number): AxiosPromise {
+		return axios.get(`${ this.rootUrl }/${ id }`);
+	}
 
-    save(data: T): AxiosPromise {
-        const { id } = data;
+	save(data: T): AxiosPromise {
+		const { id } = data;
 
-        if (!id) {
-            return axios.post(this.rootUrl, data);
-        }
+		if (!id) {
+			return axios.post(this.rootUrl, data);
+		}
 
-        return axios.put(`${ this.rootUrl }/${ id }`, data);
-    }
+		return axios.put(`${ this.rootUrl }/${ id }`, data);
+	}
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,21 +1,21 @@
-import {Eventing} from './eventing';
-import {Sync} from "./sync";
-import {Attributes} from "./attributes";
+import { Eventing } from './eventing';
+import { Sync } from "./sync";
+import { Attributes } from "./attributes";
 
 interface UserProps {
-    id?: number;
-    name?: string;
-    age?: number;
+	id?: number;
+	name?: string;
+	age?: number;
 }
 
 const ROOT_URL = 'http://localhost:3000/users';
 
 export class User {
-    public events = new Eventing();
-    public sync: Sync<UserProps> = new Sync<UserProps>(ROOT_URL);
-    public attributes: Attributes<UserProps>;
+	public events = new Eventing();
+	public sync: Sync<UserProps> = new Sync<UserProps>(ROOT_URL);
+	public attributes: Attributes<UserProps>;
 
-    constructor(attributes: UserProps) {
-        this.attributes = new Attributes<UserProps>(attributes);
-    }
+	constructor(attributes: UserProps) {
+		this.attributes = new Attributes<UserProps>(attributes);
+	}
 }
