581cf1d3d136d4829ffb41542be95b4daa7328b3
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-08-23 10:44:59 +0400

    extract attributes in special class

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,8 @@
 import {Eventing} from './eventing';
 import {Sync} from "./sync";
+import {Attributes} from "./attributes";
 
-export interface UserProps {
+interface UserProps {
     id?: number;
     name?: string;
     age?: number;
@@ -11,15 +12,10 @@ const ROOT_URL = 'http://localhost:3000/users';
 
 export class User {
     public events = new Eventing();
-    public sync = new Sync<UserProps>(ROOT_URL);
+    public sync: Sync<UserProps> = new Sync<UserProps>(ROOT_URL);
+    public attributes: Attributes<UserProps>;
 
-    constructor(private data: UserProps) {}
-
-    get(propName: string): string | number {
-        return this.data[propName];
-    }
-
-    set(newProps: UserProps): void {
-        Object.assign(this.data, newProps);
+    constructor(attributes: UserProps) {
+        this.attributes = new Attributes<UserProps>(attributes);
     }
 }
